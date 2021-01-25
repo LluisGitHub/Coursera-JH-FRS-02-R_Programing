9758e03740c3ccd9d79eebadd7a051fc9262d2c0
LluisGitHub <lluis.calvo@gmail.com>
2021-01-25 22:09:06 +0100

    Initial commit W01-01-DataTypes-Vectors-Lists.R

---
+++ W01-01-DataTypes-Vectors-Lists.R
@@ -0,0 +1,21 @@
+
+x <- c(0.5, 0.6)       ## Numeric
+print(x)
+
+x <- c(TRUE, FALSE)    ## Logical
+print(x)
+
+x <- c(T, F)           ## Logical
+print(x)
+
+x <- c("a", "b", "c")  ## Character
+print(x)
+
+x <- 9:29              ## Integer
+print(x)
+
+x <- c(1+0i, 2+4i)     ## Complex
+print(x)
+
+## vector() function:
+x <- vector("numeric", length = 10)
