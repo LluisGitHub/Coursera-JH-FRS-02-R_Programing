dd2573f371ff479255fbb03406055a8edf97a8ba
LluisGitHub <lluis.calvo@gmail.com>
2021-01-27 00:58:12 +0100

    Initial commit

---
+++ W01-03-DataTypes-Factors.R
@@ -0,0 +1,10 @@
+## Source: W01-03-DataTypes-Factors.R
+## Author: Lluis Calvo
+## Date:   27/01/2021
+
+## Factors
+## Factors are used to represent categorical data. Factors can be unordered or ordered. One can think of a factor as an integer vector where each integer has a label.
+## · Factors are treated specially by modelling functions like lm() and glm()
+##· Using factors with labels is better than using integers because factors are self-describing; having
+## a variable that has values “Male” and “Female” is better than a variable that has values 1 and 2.
+
